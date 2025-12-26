57fdd5bdc40fd3d78d5c600831ca533bb5bf5ee3
Fandra252 <fanendrachoudhary51@gmail.com>
2025-12-26 22:36:33 +0530

    complete welcome screen

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,16 +1,83 @@
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
 import React from "react";
-import { StyleSheet } from "react-native";
+import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
 
 const Welcome = () => {
   return (
     <ScreenWrapper>
-      <Typo size={30} fontWeight="700" style={{ textAlign: "center" }}>Welcome</Typo>
+      <View style={styles.container}>
+        <View>
+          <TouchableOpacity style={styles.loginButtn}>
+            <Typo fontWeight="700">Sign in</Typo>
+          </TouchableOpacity>
+
+          <Image
+            source={require("../../assets/images/welcome.png")}
+            style={styles.welcomeImage}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.footer}>
+          <View style={{ alignItems: "center" }}>
+            <Typo size={30} fontWeight={"800"}>
+              Always take control
+            </Typo>
+            <Typo size={30} fontWeight={"800"}>
+              of your finances
+            </Typo>
+          </View>
+          <View style={{ alignItems: "center", gap: 2 }}>
+            <Typo size={17} color={colors.textLight}>
+              Finances must be arranged to set a better
+            </Typo>
+            <Typo size={17} color={colors.textLight}>
+              lifestyle in future.
+            </Typo>
+          </View>
+          <View style={styles.buttonContainer}>
+            
+          </View>
+        </View>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Welcome;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    paddingTop: spacingY._7,
+  },
+  welcomeImage: {
+    width: "100%",
+    height: verticalScale(300),
+    alignSelf: "center",
+    marginTop: verticalScale(100),
+  },
+  loginButtn: {
+    alignSelf: "flex-end",
+    marginRight: spacingX._20,
+  },
+  footer: {
+    backgroundColor: colors.neutral900,
+    alignItems: "center",
+    paddingTop: verticalScale(30),
+    paddingBottom: verticalScale(45),
+    gap: spacingY._20,
+    shadowColor: "white",
+    shadowOffset: { width: 0, height: -10 },
+    elevation: 10,
+    shadowRadius: 25,
+    shadowOpacity: 0.15,
+  },
+  buttonContainer: {
+    width: "100%",
+    paddingHorizontal: spacingX._25,
+  },
+});
