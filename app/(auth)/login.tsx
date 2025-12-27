b003d7a65afe94585f1207c3b3dd78e29c2825be
Fandra252 <fanendrachoudhary51@gmail.com>
2025-12-27 19:04:29 +0530

    create Login screen and add back button

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,47 @@
+import BackButton from "@/components/BackButton";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const login = () => {
+  return (
+    <ScreenWrapper>
+      <View style={styles.container}>
+        <BackButton iconSize={28} />
+      </View>
+    </ScreenWrapper>
+  );
+};
+
+export default login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: spacingY._30,
+    paddingHorizontal: spacingX._20,
+  },
+  welcomeText: {
+    fontSize: verticalScale(20),
+    fontWeight: "bold",
+    color: colors.text,
+  },
+  forgotPassword: {
+    textAlign: "right",
+    fontWeight: "500",
+    color: colors.text,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+  },
+  footerText: {
+    textAlign: "center",
+    color: colors.text,
+    fontSize: verticalScale(15),
+  },
+});
--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -3,48 +3,70 @@ import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
 import { colors, spacingX, spacingY } from "@/constants/theme";
 import { verticalScale } from "@/utils/styling";
+import { useRouter } from "expo-router";
 import React from "react";
-import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
+import Animated, { FadeIn, FadeInDown } from "react-native-reanimated";
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <ScreenWrapper>
       <View style={styles.container}>
         <View>
-          <TouchableOpacity style={styles.loginButtn}>
+          <TouchableOpacity
+            onPress={() => router.push("/(auth)/login")}
+            style={styles.loginButtn}
+          >
             <Typo fontWeight="700">Sign in</Typo>
           </TouchableOpacity>
 
-          <Image
+          <Animated.Image
+            entering={FadeIn.duration(1000)}
             source={require("../../assets/images/welcome.png")}
             style={styles.welcomeImage}
             resizeMode="contain"
           />
         </View>
         <View style={styles.footer}>
-          <View style={{ alignItems: "center" }}>
+          <Animated.View
+            entering={FadeInDown.duration(800).springify().damping(50)}
+            style={{ alignItems: "center" }}
+          >
             <Typo size={30} fontWeight={"800"}>
               Always take control
             </Typo>
             <Typo size={30} fontWeight={"800"}>
               of your finances
             </Typo>
-          </View>
-          <View style={{ alignItems: "center", gap: 2 }}>
+          </Animated.View>
+          <Animated.View
+            entering={FadeInDown.duration(800)
+              .delay(100)
+              .springify()
+              .damping(50)}
+            style={{ alignItems: "center", gap: 2 }}
+          >
             <Typo size={17} color={colors.textLight}>
               Finances must be arranged to set a better
             </Typo>
             <Typo size={17} color={colors.textLight}>
               lifestyle in future.
             </Typo>
-          </View>
-          <View style={styles.buttonContainer}>
-            <Button>
+          </Animated.View>
+          <Animated.View
+            entering={FadeInDown.duration(800)
+              .delay(200)
+              .springify()
+              .damping(50)}
+            style={styles.buttonContainer}
+          >
+            <Button onPress={() => router.push("/(auth)/login")}>
               <Typo size={22} fontWeight={"600"} color={colors.neutral900}>
                 Get Started
               </Typo>
             </Button>
-          </View>
+          </Animated.View>
         </View>
       </View>
     </ScreenWrapper>
