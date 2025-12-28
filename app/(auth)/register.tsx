5162a5fa26b570914640295385e0284eefdbb8f4
Fandra252 <fanendrachoudhary51@gmail.com>
2025-12-28 21:30:53 +0530

    make Login and Sign Up UI

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,14 +1,152 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import BackButton from "@/components/BackButton";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/Typo";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import { useRouter } from "expo-router";
+import * as Icons from "phosphor-react-native";
+import React, { useRef, useState } from "react";
+import { Alert, Pressable, StyleSheet, View } from "react-native";
 
-const register = () => {
+const Register = () => {
+  const nameRef = useRef("");
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async () => {
+    if (
+      nameRef.current === "" ||
+      emailRef.current === "" ||
+      passwordRef.current === ""
+    ) {
+      Alert.alert("Register", "Please fill all the fields");
+      return;
+    }
+    console.log(
+      "name",
+      nameRef.current,
+      "email",
+      emailRef.current,
+      "password",
+      passwordRef.current
+    );
+    setIsLoading(true);
+    try {
+    } catch (error) {
+      Alert.alert("Register", "Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
-    <View>
-      <Text>register</Text>
-    </View>
-  )
-}
+    <ScreenWrapper>
+      <View style={styles.container}>
+        <BackButton iconSize={28} />
+        <View style={{ gap: 5, marginTop: spacingY._20 }}>
+          <Typo size={30} fontWeight={"800"}>
+            Let&apos;s
+          </Typo>
+          <Typo size={30} fontWeight={"800"}>
+            Get Started
+          </Typo>
+        </View>
+        <View style={styles.form}>
+          <Typo size={16} color={colors.textLighter}>
+            Create an account to track your expenses
+          </Typo>
+          <Input
+            placeholder="Enter your name"
+            // value={nameRef.current}
+            onChangeText={(value) => (nameRef.current = value)}
+            icon={
+              <Icons.UserIcon
+                size={verticalScale(26)}
+                color={colors.neutral300}
+                weight="fill"
+              />
+            }
+          />
+          <Input
+            placeholder="Enter your email"
+            // value={emailRef.current}
+            onChangeText={(value) => (emailRef.current = value)}
+            icon={
+              <Icons.AtIcon
+                size={verticalScale(26)}
+                color={colors.neutral300}
+                weight="fill"
+              />
+            }
+          />
+          <Input
+            placeholder="Enter your password"
+            secureTextEntry
+            // value={passwordRef.current}
+            onChangeText={(value) => (passwordRef.current = value)}
+            icon={
+              <Icons.LockIcon
+                size={verticalScale(26)}
+                color={colors.neutral300}
+                weight="fill"
+              />
+            }
+          />
+          <Typo size={14} color={colors.text} style={{ alignSelf: "flex-end" }}>
+            Already have an account?
+          </Typo>
+          <Button loading={isLoading} onPress={handleSubmit}>
+            <Typo size={21} fontWeight={"700"} color={colors.black}>
+              Sign Up
+            </Typo>
+          </Button>
+        </View>
+        <View style={styles.footer}>
+          <Typo size={15}>Already have an account?</Typo>
+          <Pressable onPress={() => router.navigate("/(auth)/login")}>
+            <Typo size={15} fontWeight={"700"} color={colors.primary}>
+              Login
+            </Typo>
+          </Pressable>
+        </View>
+      </View>
+    </ScreenWrapper>
+  );
+};
 
-export default register
+export default Register;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: spacingY._30,
+    paddingHorizontal: spacingX._20,
+  },
+  welcomeText: {
+    fontSize: verticalScale(20),
+    fontWeight: "bold",
+    color: colors.text,
+  },
+  form: {
+    gap: spacingY._20,
+  },
+  forgotPassword: {
+    textAlign: "right",
+    fontWeight: "500",
+    color: colors.text,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+  },
+  footerText: {
+    textAlign: "center",
+    color: colors.text,
+    fontSize: verticalScale(15),
+  },
+});
