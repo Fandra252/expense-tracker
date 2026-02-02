820750342a52b63867217f0de212f74b9a31f271
Fandra252 <fanendrachoudhary51@gmail.com>
2026-02-02 21:51:03 +0530

    add style  of add transaction modal

--- app/(modals)/TransactionModal.tsx
+++ app/(modals)/TransactionModal.tsx
@@ -1,15 +1,274 @@
-import ScreenWrapper from "@/components/ScreenWrapper";
-import React from "react";
-import { StyleSheet, Text } from "react-native";
+import BackButton from "@/components/BackButton";
+import Button from "@/components/Button";
+import Header from "@/components/Header";
+import ImageUpload from "@/components/ImageUpload";
+import Input from "@/components/Input";
+import ModalWrapper from "@/components/ModalWrapper";
+import Typo from "@/components/Typo";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
+import { useAuth } from "@/contexts/authContext";
+import { deleteWallet } from "@/services/walletService";
+import { TransactionType } from "@/types";
+import { scale, verticalScale } from "@/utils/styling";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import * as Icons from "phosphor-react-native";
+import React, { useEffect, useState } from "react";
+import { Alert, ScrollView, StyleSheet, View } from "react-native";
 
 const TransactionModal = () => {
+  const router = useRouter();
+  const { user } = useAuth();
+  const [transaction, setTransaction] = useState<TransactionType>({
+    type: "expense",
+    amount: 0,
+    description: "",
+    category: "",
+    date: new Date(),
+    walletId: "",
+    image: null,
+  });
+  const [loading, setLoading] = useState(false);
+
+  const oldTransaction: { name: string; image: string; id: string } =
+    useLocalSearchParams();
+
+  useEffect(() => {
+    if (oldTransaction?.id) {
+      setTransaction({
+        name: oldTransaction.name,
+        image: oldTransaction.image,
+      });
+    }
+  }, []);
+
+  const onSubmit = async () => {
+    // let { name, image } = transaction;
+    // if (!name.trim() || !image) {
+    //   Alert.alert("Wallet", "Please fill all the fields");
+    //   return;
+    // }
+    // const data: WalletType = {
+    //   name,
+    //   image,
+    //   uid: user?.uid,
+    // };
+    // if (oldTransaction?.id) data.id = oldTransaction?.id;
+    // setLoading(true);
+    // const res = await createOrUpdateWallet(data);
+    // setLoading(false);
+    // if (res.success) {
+    //   router.back();
+    // } else {
+    //   Alert.alert("Wallet", res.msg);
+    // }
+  };
+
+  const onDelete = async () => {
+    if (!oldTransaction?.id) return;
+    setLoading(true);
+    const res = await deleteWallet(oldTransaction?.id);
+    setLoading(false);
+    if (res.success) {
+      router.back();
+    } else {
+      Alert.alert("Wallet", res.msg);
+    }
+  };
+
+  const showDeleteAlert = () => {
+    Alert.alert(
+      "Confirm",
+      "Are you sure you want to do this? \nThis action will remove all the transactions related to this wallet",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        {
+          text: "Delete",
+          onPress: () => onDelete(),
+          style: "destructive",
+        },
+      ],
+    );
+  };
+
   return (
-    <ScreenWrapper>
-      <Text>TransactionModal</Text>
-    </ScreenWrapper>
+    <ModalWrapper>
+      <View style={styles.container}>
+        <Header
+          title={oldTransaction?.id ? "Update Transaction" : "New Transaction"}
+          leftIcon={<BackButton />}
+          style={{ marginBottom: spacingY._10 }}
+        />
+        <ScrollView contentContainerStyle={styles.form} showsVerticalScrollIndicator={false}>
+          <View style={styles.inputContainer}>
+            <Typo color={colors.neutral200}>Type</Typo>
+            {/* dropdown here */}
+          </View>
+          <View style={styles.inputContainer}>
+            <Typo color={colors.neutral200}>Transaction Icon</Typo>
+            <ImageUpload
+              file={transaction.image}
+              onClear={() => setTransaction({ ...transaction, image: null })}
+              onSelect={(file) =>
+                setTransaction({ ...transaction, image: file })
+              }
+              placeholder="Upload Image"
+            />
+          </View>
+        </ScrollView>
+      </View>
+      <View style={styles.footer}>
+        {oldTransaction?.id && !loading && (
+          <Button
+            onPress={showDeleteAlert}
+            style={{
+              backgroundColor: colors.rose,
+              paddingHorizontal: spacingX._15,
+            }}
+          >
+            <Icons.TrashIcon
+              size={verticalScale(20)}
+              color={colors.white}
+              weight="bold"
+            />
+          </Button>
+        )}
+        <Button onPress={onSubmit} loading={loading} style={{ flex: 1 }}>
+          <Typo color={colors.black} fontWeight={"700"}>
+            {oldTransaction?.id ? "Update Wallet" : "Add Wallet"}
+          </Typo>
+        </Button>
+      </View>
+    </ModalWrapper>
   );
 };
 
 export default TransactionModal;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: spacingX._20,
+  },
+  form: {
+    gap: spacingY._20,
+    paddingHorizontal: spacingX._15,
+    paddingBottom: spacingY._40,
+  },
+  footer: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingHorizontal: spacingX._20,
+    gap: scale(12),
+    paddingTop: spacingY._15,
+    borderTopColor: colors.neutral700,
+    marginBottom: spacingY._5,
+    borderTopWidth: 1,
+  },
+  inputContainer: {
+    gap: spacingY._10,
+  },
+  avatarContainer: {
+    position: "relative",
+    alignSelf: "center",
+  },
+  iosDropDown: {
+    flexDirection: "row",
+    height: verticalScale(54),
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: verticalScale(14),
+    borderWidth: 1,
+    color: colors.white,
+    borderColor: colors.neutral500,
+    borderRadius: radius._17,
+    borderCurve: "continuous",
+    paddingHorizontal: spacingX._15,
+  },
+  androidDropDown: {
+    // flexDirection: "row",
+    height: verticalScale(54),
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: verticalScale(14),
+    borderWidth: 1,
+    color: colors.white,
+    borderColor: colors.neutral500,
+    borderRadius: radius._17,
+    borderCurve: "continuous",
+    // paddingHorizontal: spacingX._15,
+  },
+  flexRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: spacingX._5,
+  },
+  dateInput: {
+    flexDirection: "row",
+    height: verticalScale(54),
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: colors.neutral300,
+    borderRadius: radius._17,
+    borderCurve: "continuous",
+    paddingHorizontal: spacingX._15,
+  },
+  iosDatePicker: {
+    // backgroundColor: "red",
+  },
+  datePickerButton: {
+    backgroundColor: colors.neutral700,
+    alignSelf: "flex-end",
+    padding: spacingY._7,
+    marginRight: spacingX._7,
+    paddingHorizontal: spacingX._15,
+    borderRadius: radius._10,
+  },
+  dropDownContainer: {
+    height: verticalScale(54),
+    borderWidth: 1,
+    borderColor: colors.neutral300,
+    paddingHorizontal: spacingX._15,
+    borderRadius: radius._15,
+    borderCurve: "continuous",
+  },
+  dropDownItemText: {
+    color: colors.white,
+  },
+  dropDownSelectedText: {
+    fontSize: verticalScale(14),
+    color: colors.white,
+  },
+  dropDownListContainer: {
+    backgroundColor: colors.neutral900,
+    borderRadius: radius._15,
+    borderCurve: "continuous",
+    paddingVertical: spacingY._7,
+    top: 5,
+    borderColor: colors.neutral500,
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 1,
+    shadowRadius: 15,
+    elevation: 5,
+  },
+  dropDownPlaceholder: {
+    color: colors.white,
+  },
+  dropDownItemContainer: {
+    borderRadius: radius._15,
+    marginHorizontal: spacingX._7,
+  },
+  dropDownIcon: {
+    height: verticalScale(30),
+    tintColor: colors.neutral300,
+  },
+});
