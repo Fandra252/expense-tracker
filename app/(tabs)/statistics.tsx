c57263881992cd23480102c492087d3157945d77
fanendra5869 <fanendrachoudhary51@gmail.com>
2026-03-24 23:12:39 +0530

    Work on statistics screen

--- app/(tabs)/statistics.tsx
+++ app/(tabs)/statistics.tsx
@@ -1,13 +1,87 @@
 import Header from "@/components/Header";
 import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/Typo";
 import { colors, radius, spacingX, spacingY } from "@/constants/theme";
 import { scale, verticalScale } from "@/utils/styling";
 import SegmentedControl from "@react-native-segmented-control/segmented-control";
-import React from "react";
+import React, { useState } from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
+import { BarChart } from "react-native-gifted-charts";
 
 const Statistics = () => {
   const [activeIndex, setActiveIndex] = React.useState(0);
+  const [chartData, setChartData] = useState([
+    {
+      value: 40,
+      label: "Mon",
+      spacing: scale(5),
+      labelWidth: scale(30),
+      frontColor: colors.primary,
+      topLabelComponent: () => (
+        <Typo size={10} style={{ marginBottom: 2 }} fontWeight="bold">
+          40
+        </Typo>
+      ),
+    },
+    {
+      value: 20,
+      frontColor: colors.rose,
+      topLabelComponent: () => (
+        <Typo size={10} style={{ marginBottom: 2 }} fontWeight="bold">
+          40
+        </Typo>
+      ),
+    },
+    {
+      value: 50,
+      label: "Tue",
+      spacing: scale(4),
+      labelWidth: scale(30),
+      frontColor: colors.primary,
+    },
+    { value: 40, frontColor: colors.rose },
+    {
+      value: 75,
+      label: "Wed",
+      spacing: scale(4),
+      labelWidth: scale(30),
+      frontColor: colors.primary,
+    },
+    { value: 25, frontColor: colors.rose },
+    {
+      value: 30,
+      label: "Thu",
+      spacing: scale(4),
+      labelWidth: scale(30),
+      frontColor: colors.primary,
+    },
+    { value: 60, frontColor: colors.rose },
+    {
+      value: 60,
+      label: "Fri",
+      spacing: scale(4),
+      labelWidth: scale(30),
+      frontColor: colors.primary,
+    },
+    { value: 40, frontColor: colors.rose },
+    {
+      value: 65,
+      label: "Sta",
+      spacing: scale(4),
+      labelWidth: scale(30),
+      frontColor: colors.primary,
+    },
+    { value: 30, frontColor: colors.rose },
+    {
+      value: 65,
+      label: "Sun",
+      spacing: scale(4),
+      labelWidth: scale(30),
+      frontColor: colors.primary,
+    },
+    { value: 30, frontColor: colors.rose },
+  ]);
+
   return (
     <ScreenWrapper>
       <View style={styles.container}>
@@ -18,7 +92,7 @@ const Statistics = () => {
           contentContainerStyle={{
             gap: spacingY._20,
             paddingTop: spacingY._5,
-            paddingBottom: verticalScale(100),
+            paddingBottom: verticalScale(10),
           }}
         >
           <SegmentedControl
@@ -35,6 +109,33 @@ const Statistics = () => {
             fontStyle={{ ...styles.segmentFontStyle, color: colors.white }}
           />
         </ScrollView>
+        <View style={styles.chartContainer}>
+          {chartData.length > 0 ? (
+            <BarChart
+              data={chartData}
+              barWidth={scale(13)}
+              spacing={scale(25)}
+              roundedTop
+              roundedBottom
+              hideRules
+              yAxisLabelPrefix="₹"
+              yAxisThickness={0}
+              xAxisThickness={0}
+              yAxisLabelWidth={scale(27)}
+              yAxisTextStyle={{
+                color: colors.neutral350,
+              }}
+              xAxisLabelTextStyle={{
+                color: colors.neutral350,
+                fontSize: verticalScale(12),
+              }}
+              noOfSections={3}
+              minHeight={5}
+            />
+          ) : (
+            <View style={styles.noChart} />
+          )}
+        </View>
       </View>
     </ScreenWrapper>
   );
